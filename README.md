f495f0a10d6f7986b48d0e14334541d0b923a8b9
sjpsega <ssjjpp520@126.com>
2023-02-19 00:01:37 +0800

    fix: 列表渲染 key 设置正确；checkbox value 设置正确

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # StudyMiniProgram
 Study Weixin MiniProgram
+
+* [微信小程序文档](https://developers.weixin.qq.com/miniprogram/dev/framework/)
--- pages/todo/todo.js
+++ pages/todo/todo.js
@@ -7,7 +7,7 @@ Page({
   },
   onLoad() {
     // for test
-    wx.setStorageSync('todos', []);
+    // wx.setStorageSync('todos', []);
 
     this.setData({
       todos: (wx.getStorageSync('todos') || []).map(todo => {
@@ -38,7 +38,6 @@ Page({
     });
   },
   checkboxgroupChangeHandler(e) {
-    // FIXME: e.detail.value 取值只有选中的内容，若用户输入项内容一致，后续判断选中的逻辑就会错误
     const checkeds = e.detail.value;
     console.log('checkboxgroupChangeHandler', e, checkeds);
 
@@ -48,7 +47,7 @@ Page({
       todo.isdone = false;
       for (let j = (checkeds.length - 1); j >= 0 ; j--) {
         const checked = checkeds[j];
-        if (todo.content === checked) {
+        if (todo.time == checked) {
           todo.isdone = true;
           break;
         }
@@ -61,7 +60,7 @@ Page({
         todos: sortedDodos
     });
   },
-  // FIXME: sort 后，checkboxgroud change 结果会有问题
+
   sortTodos(todos){
     // sort 内的函数返回值小于 0， firstElement 排在 secondElement 前面；
     // 返回值等于 0， firstElement，secondElement 相等顺序无关要紧； 
